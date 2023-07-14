55c65566e500f2e15d8424c9630b79d23edbffb3
alexandre-fernandez <alexandre-fernandez@gmx.com>
2023-07-14 22:52:01 +0200

    astro

--- astro-i18n/src/core/state/classes/astro-i18n.class.ts
+++ astro-i18n/src/core/state/classes/astro-i18n.class.ts
@@ -1,11 +1,12 @@
 import type Config from "@src/core/state/classes/config.class"
 
 class AstroI18n {
-	//
+	initFromDom() {
+		//
+	}
 
-	fromConfig(config: Config) {
+	initfromConfig(config: Config) {
 		console.log(config)
-		return new AstroI18n()
 	}
 }
 
