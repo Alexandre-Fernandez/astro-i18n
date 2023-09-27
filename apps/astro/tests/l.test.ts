3a75d6b488f532d01730414d691dc1eb76711eaf
alexandre-fernandez <alexandre-fernandez@gmx.com>
2023-09-27 23:06:46 +0200

    routing improvements & tests

--- apps/astro/tests/l.test.ts
+++ apps/astro/tests/l.test.ts
@@ -1,7 +1,55 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import { expect, test } from "vitest"
 import { astroI18n } from "astro-i18n"
 
-test("to-do: l", () => {
+test("Show/hide primary locale.", () => {
+	astroI18n.internals.config.showPrimaryLocale = true
+	astroI18n.route = "/en"
+	expect(astroI18n.l("/")).toBe("/en")
+	astroI18n.route = "/fr"
+	expect(astroI18n.l("/")).toBe("/fr")
+	astroI18n.internals.config.showPrimaryLocale = false
+	astroI18n.route = "/en"
+	expect(astroI18n.l("/")).toBe("/")
+	astroI18n.route = "/fr"
+	expect(astroI18n.l("/")).toBe("/fr")
+})
+
+test("Segment translations.", () => {
 	astroI18n.route = "/"
-	expect(true).toBe(true)
+	expect(astroI18n.l("/")).toBe("/")
+	astroI18n.route = "/fr"
+	expect(astroI18n.l("/")).toBe("/fr")
+	astroI18n.route = "/group/inner"
+	expect(astroI18n.l("/group/inner")).toBe("/group/inner")
+	astroI18n.route = "/fr/groupe/interieur"
+	expect(astroI18n.l("/group/inner")).toBe("/fr/groupe/interieur")
+})
+
+test("Trailing slash.", () => {
+	astroI18n.internals.config.trailingSlash = "always"
+	astroI18n.route = "/"
+	expect(astroI18n.l("/")).toBe("/")
+	astroI18n.route = "/fr"
+	expect(astroI18n.l("/")).toBe("/fr/")
+	astroI18n.route = "/group/inner"
+	expect(astroI18n.l("/group/inner")).toBe("/group/inner/")
+	astroI18n.internals.config.trailingSlash = "never"
+	astroI18n.route = "/"
+	expect(astroI18n.l("/")).toBe("/")
+	astroI18n.route = "/fr"
+	expect(astroI18n.l("/")).toBe("/fr")
+	astroI18n.route = "/group/inner"
+	expect(astroI18n.l("/group/inner")).toBe("/group/inner")
+})
+
+test("Locale detection.", () => {
+	astroI18n.route = "/group"
+	expect(astroI18n.l("/groupe/interieur")).toBe("/group/inner")
+	astroI18n.route = "/interieur"
+	expect(astroI18n.l("/groupe/interieur")).toBe("/fr/groupe/interieur")
+})
+
+test("Route parameters.", () => {
+	//
 })
