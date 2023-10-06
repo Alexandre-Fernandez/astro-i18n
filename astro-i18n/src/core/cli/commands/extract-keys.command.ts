fb50cade423d9b7fdcf51ec99b4bd04309df7821
alexandre-fernandez <alexandre-fernandez@gmx.com>
2023-10-06 22:43:44 +0200

    extract keys

---
+++ astro-i18n/src/core/cli/commands/extract-keys.command.ts
@@ -0,0 +1,22 @@
+import { toPosixPath } from "@lib/async-node/functions/path.functions"
+import { isDirectory } from "@lib/async-node/functions/fs.functions"
+import InvalidCommand from "@src/core/cli/errors/invalid-command.error"
+import RootNotFound from "@src/core/config/errors/root-not-found.error"
+import { astroI18n } from "@src/core/state/singletons/astro-i18n.singleton"
+import type { Command, ParsedArgv } from "@lib/argv/types"
+
+const cmd = {
+	name: "extract:keys",
+	options: [],
+} as const satisfies Command
+
+export async function extractKeys({ command, args }: ParsedArgv) {
+	if (command !== cmd.name) throw new InvalidCommand()
+
+	const root = await toPosixPath(args[0] || process.cwd())
+	if (!(await isDirectory(root))) throw new RootNotFound()
+
+	await astroI18n.initialize()
+}
+
+export default cmd
--- astro-i18n/src/core/cli/constants/cli-patterns.constants.ts
+++ astro-i18n/src/core/cli/constants/cli-patterns.constants.ts
@@ -4,3 +4,7 @@ import { PACKAGE_NAME } from "@src/constants/meta.constants"
 export const GENERATED_TYPES_PATTERN = Regex.fromString(
 	`\\/{2}\\s+###>\\s+${PACKAGE_NAME}\\/type-generation\\s+###\\n([\\s\\S]*)\\/{2}\\s+###<\\s+${PACKAGE_NAME}\\/type-generation ###`,
 )
+
+export const TRANSLATION_FUNCTION_PATTERN = new Regex(
+	/t\s*\(\s*["'`]([\S\s]*?)["'`]\s*[),]/g,
+)
